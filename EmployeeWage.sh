bcf542a14b6879321988194bbf43477a1d8dbd81
mouna07 <mounamc261@gmail.com>
2021-01-19 20:31:40 +0530

    Added:wage of month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,19 +2,23 @@
 
 echo "*********Welocome to the Employee Wage computation Program*************"
 
-
 isPartTime=1
 isFullTime=2
 empRatePerHr=20
-empcheck=$((RANDOM%3))
+totalSalary=0
+numWorkingDays=20
 
-case $empcheck in
-	$isPartTime)
-		empHrs=4
-	$isFullTime)
-		empHrs=8
-	*)
-		empHrs=0
-esac
-salary=$(($empHrs*$empRatePerHr))
-
+for (( day=1; day<=$numWorkingDays; day++))
+do
+	empcheck=$((RANDOM%3))
+	case $empcheck in
+		$isPartTime)
+			empHrs=4
+		$isFullTime)
+			empHrs=8
+		*)
+			empHrs=0
+	esac
+	salary=$(($empHrs*$empRatePerHr))
+	totalSalary=$(($totalSalary+$salary))
+done
